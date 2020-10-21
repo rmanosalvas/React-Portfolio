26e4673935c731895734002e258ec4b239d4eaf0
Ricardo <manosalvas2013@gmail.com>
2020-10-21 18:54:26 -0400

    router complete

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { Container } from 'react-bootstrap';
+
+// Components //
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+// Pages // 
+import Home from './Pages/Home';
+import Portfolio from './Pages/Portfolio';
+import Contact from './Pages/Contact';
+
+
+function App() {
+  return (
+    <>
+      <Router>
+          <Header />
+          <Switch>
+            <Container>
+                <Route exact path='/' name='Home' component={Home} />
+                <Route path='/portfolio' name='Portfolio' component={Portfolio} />
+                <Route path='/contact' name='Contact' component={Contact} />  
+              </Container>       
+          </Switch>
+          {/* <Footer /> */}
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Component } from "react";
+
+
+function Header () {
+    return (
+        <div>
+            <Navbar bg="dark" expand="lg">
+            <Navbar.Brand href="/">Ricardo Manosalvas</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
+
+
