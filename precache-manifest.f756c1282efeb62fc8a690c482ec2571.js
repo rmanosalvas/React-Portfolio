45394130e003a15651711970c1dff02c8f24d857
Ricardo <manosalvas2013@gmail.com>
2020-10-22 22:06:22 -0400

    Updates

---
+++ precache-manifest.f756c1282efeb62fc8a690c482ec2571.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b6e78d65427d77cec502fe0b898a6f",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "c35c7b4cbd4f9519f9e6",
+    "url": "/React_Portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1248a01526fe50150bf0",
+    "url": "/React_Portfolio/static/css/main.693308a8.chunk.css"
+  },
+  {
+    "revision": "c35c7b4cbd4f9519f9e6",
+    "url": "/React_Portfolio/static/js/2.f957067b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Portfolio/static/js/2.f957067b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1248a01526fe50150bf0",
+    "url": "/React_Portfolio/static/js/main.0ce04830.chunk.js"
+  },
+  {
+    "revision": "32135041e8b8f4902bc9",
+    "url": "/React_Portfolio/static/js/runtime-main.3d088758.js"
+  },
+  {
+    "revision": "5ab3b3ca15e0b98fed19dc7fbd316c5f",
+    "url": "/React_Portfolio/static/media/BurgerLog.5ab3b3ca.png"
+  },
+  {
+    "revision": "5c0d8454bd7f248e1e9c0bcf3411c394",
+    "url": "/React_Portfolio/static/media/DateMatch.5c0d8454.png"
+  },
+  {
+    "revision": "039cfdc5a402f65e387b4ad72b8a7116",
+    "url": "/React_Portfolio/static/media/EmployeeManager.039cfdc5.png"
+  },
+  {
+    "revision": "9188aaf6c9785abf2bd3649507f29cef",
+    "url": "/React_Portfolio/static/media/FitnessGuru.9188aaf6.png"
+  },
+  {
+    "revision": "c24a472b70f215b4acc82f4bd5a15624",
+    "url": "/React_Portfolio/static/media/FitnessTracker.c24a472b.png"
+  },
+  {
+    "revision": "7a766c7c2a8ffc4c189a5974fb20c167",
+    "url": "/React_Portfolio/static/media/Headshot.7a766c7c.jpeg"
+  },
+  {
+    "revision": "dcdb051658207d1f57b937af64724274",
+    "url": "/React_Portfolio/static/media/NoteTaker.dcdb0516.png"
+  },
+  {
+    "revision": "b077bd97985e8c726e1a951af229db2b",
+    "url": "/React_Portfolio/static/media/Resume.b077bd97.png"
+  }
+]);
